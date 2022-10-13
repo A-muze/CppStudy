8b9791e5714a93016c94ddc46ed46d37e92ab23a
Amuze <liminw417@gmail.com>
2022-10-13 23:46:18 +0900

    c++ 입출력

---
+++ cppStudy01_IO.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+    // <iostream>은 cout, cin, endl과 같은 입출력에 필요한 것들을 포함하고 있습니다 
+
+    int n;
+
+    // 입력
+    std::cin >> n;
+
+    // 출력
+    std::cout << n;
+
+    return 0;
+}
